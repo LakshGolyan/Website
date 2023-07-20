99937b5d8bc35f39e62f356b61545099f7fc5f74
LakshGolyan <140082145+LakshGolyan@users.noreply.github.com>
2023-07-20 19:03:42 +0545

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function changecolor(){
+    document.getElementById("my_body").style.backgroundColor="red";
+    document.getElementById("heading").style.color="yellow";
+    document.getElementById("heading").style.fontStyle="italic";
+}
+function backtonormal(){
+    document.getElementById("my_body").style.backgroundColor="black";
+    document.getElementById("heading").style.color="white";
+    document.getElementById("heading").style.fontStyle="normal";
+    
+}
